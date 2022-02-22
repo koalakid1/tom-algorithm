6b9484948e8e94dde537c62e55a982320497fac1
koalakid1 <koalakid154@gmail.com>
2022-02-22 14:34:15 +0900

    1259 - 팰린드롭수

---
+++ BOJ/class2/1259.py
@@ -0,0 +1,20 @@
+# 어떤 단어를 뒤에서부터 읽어도 똑같다면 그 단어를 팰린드롬이라고 한다. 'radar', 'sees'는 팰린드롬이다.
+
+# 수도 팰린드롬으로 취급할 수 있다. 수의 숫자들을 뒤에서부터 읽어도 같다면 그 수는 팰린드롬수다. 121, 12421 등은 팰린드롬수다. 123, 1231은 뒤에서부터 읽으면 다르므로 팰린드롬수가 아니다. 또한 10도 팰린드롬수가 아닌데, 앞에 무의미한 0이 올 수 있다면 010이 되어 팰린드롬수로 취급할 수도 있지만, 특별히 이번 문제에서는 무의미한 0이 앞에 올 수 없다고 하자.
+
+import sys
+input = sys.stdin.readline
+
+
+def 팰린드롭수(num_str):
+    num_str_reverse = num_str[::-1]
+    return "yes" if num_str == num_str_reverse else "no"
+
+
+if __name__ == "__main__":
+    while True:
+        num_str = input().strip()
+        if num_str == "0":
+            break
+
+        print(팰린드롭수(num_str))
